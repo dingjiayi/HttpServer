a9fca67cfe6aaae8c554615ea653fbfcccd22d33
dingjiayi <1548534563@qq.com>
2017-04-15 14:59:57 +0800

    @author: dingjiayi @comment:      1. add apis.py     deal APIError      2. add handlers.py  handle url      3. add coroweb.py only file

---
+++ www/coroweb.py
@@ -0,0 +1,9 @@
+
+# -*- coding: utf-8 -*-
+import asyncio, os, inspect, logging, functools
+
+from urllib import parse
+
+from aiohttp import web
+
+from apis import APIError
---
+++ www/handlers.py
@@ -0,0 +1,6 @@
+
+# -*- coding:utf-8 -*-
+
+__author__ = 'dingjiayi'
+
+' url handlers '
