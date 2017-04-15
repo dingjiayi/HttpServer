fd2e060a3d9581b84ea3422c77e7dd40ae661199
dingjiayi <1548534563@qq.com>
2017-04-15 11:35:40 +0800

    @author: dingjiayi @comment:      remove bug      add models

--- www/models.py
+++ www/models.py
@@ -1,5 +1,3 @@
-#!/usr/bin/ python3.6
-
 # -*- coding:utf-8 -*-
 
 '''
@@ -9,16 +7,45 @@ Models for user, blog, comment.
 __authro__ = 'dingjiayi'
 
 import time, uuid
-
-# from orm import Model, StringField, BooleanField, FloatField, TextField
+from orm import Model
+from orm import StringField
+from orm import BooleanField
+from orm import TextField
+from orm import FloatField
 
 def next_id():
     return '%015d%s' % (int(time.time()*1000), uuid.uuid4().hex)
 
-a= time.time()
-print(a)
-b = uuid.uuid4()
-c = b.hex
-print(b)
-print(c)
-print(next_id())
+class User(Model):
+    __table__ = 'users'
+
+    id = StringField(primary_key=True, default=next_id, ddl='vatchar(50)')
+    email = StringField(ddl='varchar(50)')
+    passwd = StringField(ddl = 'varchar(50)')
+    admin = BooleanField()
+    name = StringField(ddl = 'varchar(50)')
+    image = StringField(ddl = 'vatchar(500)')
+    created_at = FloatField(default=time.time)
+
+class Blog(Model):
+    __table__ = 'blogs'
+
+    id = StringField(primary_key=True, default=next_id, ddl = 'varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    name = StringField(ddl = 'varchar(50)')
+    summary = StringField(ddl = 'varchar(200)')
+    content = TextField()
+    created_at = FloatField(default=time.time)
+
+class Comment(Model):
+    __table__ = 'comments'
+
+    id = StringField(primary_key = True, default = next_id, ddl = 'varchar(50)')
+    blog_id = StringField(ddl = 'varchar(50)')
+    user_id = StringField(ddl = 'varchar(50)')
+    user_name = StringField(ddl = 'varchar(50)')
+    user_image = StringField(ddl = 'vatchar(500)')
+    content = TextField()
+    created_at = FloatField(default = time.time)
--- www/orm.py
+++ www/orm.py
@@ -5,42 +5,46 @@ __author__ = 'dingjiayi'
 import asyncio, logging
 import aiomysql
 
-def log(sql, args = ()):
+
+def log(sql, args=()):
     logging.info('SQL: %s' % sql)
+
 
 async def create_pool(loop, **kw):
     logging.info("create database connection pool...")
     global __pool
     __pool = await aiomysql.create_pool(
-        host = kw.get('host', 'localhost'),
-        port = kw.get('port', 3306),
-        user = kw['user'],
-        password = kw['password'],
-        db = kw['db'],
-        charset = kw.get('charset', 'utf8'),
-        autocommit = kw.get('atuocommit', True),
-        maxsize = kw.get('maxsize', 10),
-        minsize = kw.get('minsize', 1),
-        loop = loop
+        host=kw.get('host', 'localhost'),
+        port=kw.get('port', 3306),
+        user=kw['user'],
+        password=kw['password'],
+        db=kw['db'],
+        charset=kw.get('charset', 'utf8'),
+        autocommit=kw.get('atuocommit', True),
+        maxsize=kw.get('maxsize', 10),
+        minsize=kw.get('minsize', 1),
+        loop=loop
     )
 
-async def select(sql, args, size = None):
-        log(sql, args)
-        global __pool
-        async with __pool.get() as conn:
-            async with conn.cursor(aiomysql.DictCursor) as cur:
-                await cur.execute(sql.replace('?', '%s', args or ()))
-            if size:
-                rs = await  cur.fetchmany(size)
-            else:
-                rs = await cur.fetchall()
 
-        logging.info("rows returned: %s" % len(rs))
-        return rs
+async def select(sql, args, size=None):
+    log(sql, args)
+    global __pool
+    async with __pool.get() as conn:
+        async with conn.cursor(aiomysql.DictCursor) as cur:
+            await cur.execute(sql.replace('?', '%s', args or ()))
+        if size:
+            rs = await  cur.fetchmany(size)
+        else:
+            rs = await cur.fetchall()
+
+    logging.info("rows returned: %s" % len(rs))
+    return rs
+
 
 async def execute(sql, args):
     log(sql)
-    async with __pool.get() as coon:
+    async with __pool.get() as conn:
         if not autocommit:
             await conn.begit()
         try:
@@ -55,11 +59,13 @@ async def execute(sql, args):
             raise
         return affected
 
+
 def create_args_string(num):
     L = []
     for n in range(num):
         L.append('?')
     return ','.join(L)
+
 
 class Field(object):
     def __init__(self, name, column_type, primary_key, default):
@@ -71,34 +77,46 @@ class Field(object):
     def __str__(self):
         return '<%s, %s:%s>' % (self.__class__.__name__, self.column_type, self.name)
 
-class StringFiled(Field):
-    def __init__(self, name = None, primary_key = False, default = None, ddl = 'varchar(100)'):
+
+class StringField(Field):
+
+    def __init__(self, name=None, primary_key=False, default=None, ddl='varchar(100)'):
         super().__init__(name, ddl, primary_key, default)
 
+
 class BooleanField(Field):
-    def __init__(self, name = None, default = False):
-        supper().__init(name, 'boolean', False, default)
+
+    def __init__(self, name=None, default=False):
+        super().__init__(name, 'boolean', False, default)
+
 
 class IntegerField(Field):
-    def __init__(self, name = None, primary_key = False, default = 0):
+
+    def __init__(self, name=None, primary_key=False, default=0):
         super().__init__(name, 'bigint', primary_key, default)
 
+
 class FloatField(Field):
-    def __init__(self, name = None, primary_key = False, default = 0.0):
+
+    def __init__(self, name=None, primary_key=False, default=0.0):
         super().__init__(name, 'real', primary_key, default)
 
+
 class TextField(Field):
-    def __init__(self, name = None, default = None):
+
+    def __init__(self, name=None, default=None):
         super().__init__(name, 'text', False, default)
 
+
 class ModelMetaclass(type):
+
     # 清除Model类本身
     def __new__(cls, name, bases, attrs):
         if name == 'Model':
             return type.__new__(cls, name, bases, attrs)
         # 获取table名称
         tableName = attrs.get("__table__", None) or name
-        logging.info('found model: %s (table: %s)' %(name, tableName))
+        logging.info('found model: %s (table: %s)' % (name, tableName))
         # 获取所有的Field和主键名：
         mappings = dict()
         fields = []
@@ -108,7 +126,7 @@ class ModelMetaclass(type):
                 logging.info("found mapping：%s ==> %s" % (k, v))
                 mappings[k] = v
                 if v.primary_key:
-                # 找到主键
+                    # 找到主键
                     if primaryKey:
                         raise RuntimeError("Duplicate primary key for field: %s", k)
                     primaryKey = k
@@ -125,13 +143,17 @@ class ModelMetaclass(type):
         attrs['__fields__'] = fields  # 除主键外的属性名
         # 构造默认的SELECT INSERT UPDATE 和 DELETE 语句
         attrs['__select__'] = 'select `%s`, %s from `%s`' % (primaryKey, ','.join(escaped_fields), tableName)
-        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values(%s)' % (tableName, ','.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
-        attrs['__update__'] = 'update `%s` set %s where `%s` =?' % (tableName, ','.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
+        attrs['__insert__'] = 'insert into `%s` (%s, `%s`) values(%s)' % (
+        tableName, ','.join(escaped_fields), primaryKey, create_args_string(len(escaped_fields) + 1))
+        attrs['__update__'] = 'update `%s` set %s where `%s` =?' % (
+        tableName, ','.join(map(lambda f: '`%s`=?' % (mappings.get(f).name or f), fields)), primaryKey)
         attrs['__delete__'] = 'delete from `%s` where `%s`=?' % (tableName, primaryKey)
 
         return type.__new__(cls, name, bases, attrs)
 
-class Model(dict, metaclass = ModelMetaclass):
+
+class Model(dict, metaclass=ModelMetaclass):
+
     def __init__(self, **kw):
         super(Model, self).__init__(**kw)
 
@@ -159,7 +181,7 @@ class Model(dict, metaclass = ModelMetaclass):
         return value
 
     @classmethod
-    async def findAll(cls, where = None, args = None, **kw):
+    async def findAll(cls, where=None, args=None, **kw):
         'find objects by where clause'
         sql = [cls.__select__]
         if where:
@@ -187,7 +209,7 @@ class Model(dict, metaclass = ModelMetaclass):
         return [cls(**r) for r in rs]
 
     @classmethod
-    async def findNumber(cls, selectField, where = None, args = None):
+    async def findNumber(cls, selectField, where=None, args=None):
         'find number by select and where.'
         sql = ['select %s _num_ from `%s` ' % (selectField, cls.__table__)]
         if where:
@@ -200,7 +222,7 @@ class Model(dict, metaclass = ModelMetaclass):
 
     @classmethod
     async def find(cls, pk):
-        ' find object by primary key.'
+        ' find object by primary key. '
         rs = await select('%s where `%s`=?' % (cls.__select__, cls.__primart_key__), [pk], 1)
         if len(rs) == 0:
             return None
@@ -211,25 +233,17 @@ class Model(dict, metaclass = ModelMetaclass):
         args.append(self.getValueOrDefault(self.__primary_key__))
         rows = await execute(self.__insert__, args)
         if rows != 1:
-            logging.warn('failed to insert record: affected rows: %s' % rows)
+            logging.warning('failed to insert record: affected rows: %s' % rows)
 
     async def update(self):
         args = list(map(self.getValue, self.__fields__))
         args.append(self.getValue(self.__primary_key__))
         rows = await execute(self.__update__, args)
         if rows != 1:
-            logging.warn('failed to update by primary key: affected rows: %s' % rows)
+            logging.warning('failed to update by primary key: affected rows: %s' % rows)
 
     async def remove(self):
         args = [self.getValue(self.__primary_key__)]
         rows = await execute(self.__delete__, args)
         if rows != 1:
-            logging.warn('failed to remove by primary key: affected rows: %s' % rows)
-
-class User(Model):
-    __table__ = "users"
-
-    id = IntegerField(primary_key = True)
-    name = StringFiled()
-
-# user = User(id = 1, name = 'dingjiayi')
+            logging.warning('failed to remove by primary key: affected rows: %s' % rows)
